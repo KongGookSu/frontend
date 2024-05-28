df3702fa9e0f28ea5a89793ec21f6aa60b06e615
junyeokk <cjh4302@gmail.com>
2024-05-29 01:25:52 +0900

    chore: prettier로 인한 코드 재수정 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import { Routes, Route } from "react-router-dom";
-import { GlobalStyle } from "./styles/globals.ts";
+
 import { MainLayout } from "@/components/layout/MainLayout";
-import HomePage from "@/pages/HomePage";
-import LoginPage from "@/pages/LoginPage";
-import BookSearchPage from '@/pages/BookSearchPage';
+
+import BookDetailPage from "@/pages/BookDetailPage";
 import BookEnrollPage from "@/pages/BookEnrollPage";
 import BookRentPage from "@/pages/BookRentPage";
-import BookDetailPage from "@/pages/BookDetailPage";
-import ChatListPage from "@/pages/ChatListPage";
+import BookSearchPage from "@/pages/BookSearchPage";
 import ChatDetailPage from "@/pages/ChatDetailPage";
+import ChatListPage from "@/pages/ChatListPage";
+import HomePage from "@/pages/HomePage";
+import LoginPage from "@/pages/LoginPage";
+
+import { GlobalStyle } from "@/styles/globals.ts";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/search" element={<BookSearchPage />} />
-          <Route path="/enroll" element={<BookEnrollPage />} />
-          <Route path="/book" element={<BookRentPage />} />
-          <Route path="/book/:bookId" element={<BookDetailPage />} />
-          <Route path="/chat" element={<ChatListPage />} />
-          <Route path="/chat/:chatId" element={<ChatDetailPage />} />
-        </Route>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/search" element={<BookSearchPage />} />
+                    <Route path="/enroll" element={<BookEnrollPage />} />
+                    <Route path="/book" element={<BookRentPage />} />
+                    <Route path="/book/:bookId" element={<BookDetailPage />} />
+                    <Route path="/chat" element={<ChatListPage />} />
+                    <Route path="/chat/:chatId" element={<ChatDetailPage />} />
+                </Route>
+            </Routes>
+        </>
+    );
 };
 
 export default App;
