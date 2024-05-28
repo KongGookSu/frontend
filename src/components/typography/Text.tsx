34efb4705d1c3dedeb990ce724008ee8111e0f71
junyeokk <cjh4302@gmail.com>
2024-05-29 01:29:07 +0900

    feat: 텍스트 공통 컴포넌트 구현 (#3)

---
+++ src/components/typography/Text.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export interface IText {
+    size: "xxxs" | "xxxs" | "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
+    weight: "bold" | "normal";
+    variant?: "primary" | "secondary" | "background" | "success" | "warning" | "black" | "white";
+}
+
+export const Text = styled.span<IText>`
+    font-weight: ${(props) => props.weight ?? "normal"};
+
+    font-size: ${(props) => {
+        switch (props.size) {
+            case "xxxs":
+                return "8px";
+            case "xxs":
+                return "10px";
+            case "xs":
+                return "12px";
+            case "s":
+                return "14px";
+            case "m":
+                return "16px";
+            case "l":
+                return "18px";
+            case "xl":
+                return "22px";
+            case "xxl":
+                return "24px";
+            case "xxxl":
+                return "36px";
+            default:
+                return props.size;
+        }
+    }};
+
+    color: ${(props) => {
+        switch (props.variant) {
+            case "primary":
+                return "#9045DC";
+            case "secondary":
+                return "#AE7AE3";
+            case "background":
+                return "f2eeff";
+            case "success":
+                return "#4c935e";
+            case "warning":
+                return "cc4545";
+            case "black":
+                return "#000000";
+            case "white":
+                return "#ffffff";
+            default:
+                return props.variant;
+        }
+    }};
+`;
