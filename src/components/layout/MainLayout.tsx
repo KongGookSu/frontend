ace57ecd3e377a417f9484194e4827a96770700b
junyeokk <cjh4302@gmail.com>
2024-05-29 23:44:56 +0900

    style: 모든 페이지 패딩 30px 추가 및 패딩이 레이아웃에 영향을 미치지 않도록 box-sizing을 border-box로 설정 (#4)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,16 +3,20 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 export const Main = styled.main`
-  width: min(100%, 700px);
-  margin: 0px auto;
+    width: min(100%, 700px);
+    margin: 0px auto;
+    min-height: 100vh;
+    background-color: #f2eeff;
+    padding: 30px;
+    box-sizing: border-box;
 `;
 
 export const MainLayout = () => {
-  return (
-    <>
-      <Main>
-        <Outlet />
-      </Main>
-    </>
-  );
+    return (
+        <>
+            <Main>
+                <Outlet />
+            </Main>
+        </>
+    );
 };
