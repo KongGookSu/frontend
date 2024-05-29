c034dcabe2ef8ee0f0dbbc58d468d42ab5c11cca
junyeokk <cjh4302@gmail.com>
2024-05-29 22:15:15 +0900

    style: 폰트 전역 설정 조정 및 디자인 변경 (#3)

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -3,9 +3,10 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
 
-  body {
+  * {
     margin: 0;
     padding: 0;
     font-family: 'Pretendard', sans-serif;
+    letter-spacing: -2%;
   }
 `;
