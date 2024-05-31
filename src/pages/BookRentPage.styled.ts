a664f22bbe322a77911709f7f47ebb669a636e74
junyeokk <cjh4302@gmail.com>
2024-05-31 22:32:02 +0900

    feat: GPS 기반으로 위치 자동 설정되는 것을 보여주는 컴포넌트 구현 (#6)

---
+++ src/pages/BookRentPage.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BookRentPageWrapper = styled.div`
+    background-color: #f2eeff;
+`;
+
+export const BookList = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 20px;
+`;
--- src/pages/BookRentPage.tsx
+++ src/pages/BookRentPage.tsx
@@ -1,9 +1,27 @@
+import { BookItem } from "@/components/display/BookItem";
+import { Location } from "@/components/display/Location";
+import { Search } from "@/components/display/Search";
 import { TopBar } from "@/components/display/TopBar";
 
+import { useSearch } from "@/hooks/useSearch";
+
+import { BookList, BookRentPageWrapper } from "./BookRentPage.styled";
+
 export default function BookRentPage() {
+    const { handleSearch, filteredBooks } = useSearch();
+
     return (
         <>
             <TopBar title="책 대여" />
+            <BookRentPageWrapper>
+                <Location />
+                <Search onSearch={handleSearch} />
+                <BookList>
+                    {filteredBooks.map((book) => (
+                        <BookItem key={book.id} book={book} />
+                    ))}
+                </BookList>
+            </BookRentPageWrapper>
         </>
     );
 }
