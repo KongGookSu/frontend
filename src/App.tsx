743b9b8e161c4bb85ad49df52f5ec85065ce3738
junyeokk <cjh4302@gmail.com>
2024-06-02 01:29:50 +0900

    feat: 로그인 되어있지 않은 상태에서 로그인 페이지 이외의 링크로 들어갈 수 없도록 구현 (#21)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { MainLayout } from "@/components/layout/MainLayout";
+import ProtectedRoute from "@/components/navigation/ProtectedRoute";
 
 import BookDetailPage from "@/pages/BookDetailPage";
 import BookEnrollPage from "@/pages/BookEnrollPage";
@@ -24,23 +25,25 @@ const App: React.FC = () => {
 
     useEffect(() => {
         setUsers(userData);
-        setCurrentUser(userData.find((user) => user.id === 1) as User); // Add type assertion
+        setCurrentUser(userData.find((user) => user.id === 1) as User);
     }, [setUsers, setCurrentUser]);
 
     return (
         <>
             <GlobalStyle />
             <Routes>
-                <Route path="/" element={<MainLayout />}>
-                    <Route index path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/search" element={<BookSearchPage />} />
-                    <Route path="/enroll" element={<BookEnrollPage />} />
-                    <Route path="/book" element={<BookRentPage />} />
-                    <Route path="/book/:bookId" element={<BookDetailPage />} />
-                    <Route path="/chat" element={<ChatListPage />} />
-                    <Route path="/chat/:chatId" element={<ChatDetailPage />} />
-                    <Route path="/user/:userId" element={<UserPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<ProtectedRoute />}>
+                    <Route element={<MainLayout />}>
+                        <Route index path="/" element={<HomePage />} />
+                        <Route path="/search" element={<BookSearchPage />} />
+                        <Route path="/enroll" element={<BookEnrollPage />} />
+                        <Route path="/book" element={<BookRentPage />} />
+                        <Route path="/book/:bookId" element={<BookDetailPage />} />
+                        <Route path="/chat" element={<ChatListPage />} />
+                        <Route path="/chat/:chatId" element={<ChatDetailPage />} />
+                        <Route path="/user/:userId" element={<UserPage />} />
+                    </Route>
                 </Route>
             </Routes>
         </>
